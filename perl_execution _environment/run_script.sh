9491c73ee688ce8bce09eadfefca9545f080e05f
WeixiangYAN <80093601+WeixiangYAN@users.noreply.github.com>
2023-08-30 22:30:41 -0700

    Update run_script.sh

--- perl_execution _environment/run_script.sh
+++ perl_execution _environment/run_script.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-#*******************Use them only the first time.************************
-
 CONDA_BASE=$(conda info --base)
 
-conda create -n perl_envss python=3.8 -y
+#*******************Use them only the first time.************************
 
-. $CONDA_BASE/etc/profile.d/conda.sh && conda activate perl_envss && conda install -c conda-forge perl -y
+conda create -n perl_env python=3.8 -y
 
-$CONDA_BASE/envs/perl_envss/bin/perl -v   # Print the perl version information and check if the installation is successful.
+. $CONDA_BASE/etc/profile.d/conda.sh && conda activate perl_env && conda install -c conda-forge perl -y
+
+$CONDA_BASE/envs/perl_env/bin/perl -v   # Print the perl version information and check if the installation is successful.
 
 #*******************Use them only the first time.************************
 
 
-$CONDA_BASE/envs/perl_envss/bin/python run_perl_scripts.py --input_txt /root/autodl-tmp/perl.txt --output_folder /root/autodl-tmp/perl --output_txt /root/autodl-tmp/output_results.txt
+$CONDA_BASE/envs/perl_env/bin/python run_perl_scripts.py --input_txt /root/autodl-tmp/perl.txt --output_folder /root/autodl-tmp/perl --output_txt /root/autodl-tmp/output_results.txt
 
 
